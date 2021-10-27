6ed9c64996a30fa01d7a796f234f02179aa03420
oussama DAHMAZ <oussama.dahmaz@gmail.com>
2021-10-27 20:21:50 +0200

    first import

---
+++ apps/yadoms-cs/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  displayName: 'yadoms-cs',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.vue$': 'vue3-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
+  coverageDirectory: '../../coverage/apps/yadoms-cs',
+  snapshotSerializers: ['jest-serializer-vue'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      babelConfig: '<rootDir>/babel.config.js',
+    },
+    'vue-jest': {
+      tsConfig: 'apps/yadoms-cs/tsconfig.spec.json',
+      babelConfig: 'apps/yadoms-cs/babel.config.js',
+    },
+  },
+};
